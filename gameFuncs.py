94a15f19bd630442c7b8b8472e6cd7f00bc5d86e
Kidane Gabay <puffertron@gmail.com>
2023-11-04 19:05:31 -0400

    lil fix

--- gameFuncs.py
+++ gameFuncs.py
@@ -87,33 +87,6 @@ class GameFuncs():
             if not pg.mouse.get_pressed()[0]:
                 cls.chosenPlanet = None
                 # TODO update state mode
-    
-
-def intersectTwoCircles(x1,y1,r1,x2,y2,r2):
-    r1 = r1
-    r2 = r2
-    centerdx = x1 - x2
-    centerdy = y1 - y2
-
-    R = math.sqrt(centerdx * centerdx + centerdy * centerdy)
-    R2 = R*R
-    R4 = R2*R2
-    a = (r1*r1 - r2*r2) / (2 * R2)
-    r2r2 = (r1*r1 - r2*r2)
-    c = math.sqrt(2 * (r1*r1 + r2*r2) / R2 - (r2r2 * r2r2) / R4 - 1)
-
-    fx = (x1+x2) / 2 + a * (x2 - x1)
-    gx = c * (y2 - y1) / 2
-    ix1 = fx + gx
-    ix2 = fx - gx
-
-    fy = (y1+y2) / 2 + a * (y2 - y1)
-    gy = c * (x1 - x2) / 2
-    iy1 = fy + gy
-    iy2 = fy - gy
-
-    return (pg.Vector2(ix1, iy1), pg.Vector2(ix2, iy2))
-
 
     missile: Missile = None
     @classmethod
@@ -142,3 +115,29 @@ def intersectTwoCircles(x1,y1,r1,x2,y2,r2):
         # Only keep MaxLen old positions
         if len(cls.missile.oldX) > cls.missile.oldXMaxLen:
             cls.missile.oldX.pop(1)
+
+
+def intersectTwoCircles(x1,y1,r1,x2,y2,r2):
+    r1 = r1
+    r2 = r2
+    centerdx = x1 - x2
+    centerdy = y1 - y2
+
+    R = math.sqrt(centerdx * centerdx + centerdy * centerdy)
+    R2 = R*R
+    R4 = R2*R2
+    a = (r1*r1 - r2*r2) / (2 * R2)
+    r2r2 = (r1*r1 - r2*r2)
+    c = math.sqrt(2 * (r1*r1 + r2*r2) / R2 - (r2r2 * r2r2) / R4 - 1)
+
+    fx = (x1+x2) / 2 + a * (x2 - x1)
+    gx = c * (y2 - y1) / 2
+    ix1 = fx + gx
+    ix2 = fx - gx
+
+    fy = (y1+y2) / 2 + a * (y2 - y1)
+    gy = c * (x1 - x2) / 2
+    iy1 = fy + gy
+    iy2 = fy - gy
+
+    return (pg.Vector2(ix1, iy1), pg.Vector2(ix2, iy2))
