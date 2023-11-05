fb15f9cc5d43fdc27e3759d16bb41bb774b6ac1d
Ciaran-McDJ <ciaranmcdj@gmail.com>
2023-11-05 11:39:38 -0500

    working on potential calcs

--- potentialCalcs.py
+++ potentialCalcs.py
@@ -3,5 +3,18 @@ from state import State
 
 
 def makePotentialMatrix():
-    #Uses current planet configuration to 
-    pass
+    #Uses current planet configuration to calc potential
+    #TODO - make differential version for better computing speed
+    State.potentialMatrix(np.zeros(shape=(config.playFieldWidth,config.playFieldHeight)))
+    for planet in State.planetGroups:
+        matrixToAdd = np.zeros(shape=(config.playFieldWidth,config.playFieldHeight))
+        planet.rect.center
+        planet.radius
+
+        #Get coords of planet for matrix coords
+        planetx = planet.rect.center[0]
+        planety = planet.rect.center[1]
+
+        # for pixxpos in range(TODO):
+        #     for pixypos in range
+        
